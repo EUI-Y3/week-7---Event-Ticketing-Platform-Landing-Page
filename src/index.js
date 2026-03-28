ad15053525195a7f989a33fc1da30797cf7e9df1
Samia Ahmed <23-101162@students.eui.edu.eg>
2026-03-28 15:20:00 +0200

    adding routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Routess from './routes';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+  <Routess/>
   </React.StrictMode>
 );
-
+   
